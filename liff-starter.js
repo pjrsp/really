96a9f61d0de89c248de1c4aba7e79d403bbcd9ee
pjrsp <59156916+pjrsp@users.noreply.github.com>
2020-01-14 10:09:39 +0700

    Add files via upload

---
+++ liff-starter.js
@@ -0,0 +1,43 @@
+window.onload = function(e) {
+    liff.init(function(data) {
+        initializeApp();
+    });
+};
+
+function initializeApp() {
+    document.getElementById('sendmessagebutton').addEventListener('click', function() {
+        var x;
+        var msg = document.getElementById('bchcode').value;
+
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    x = this.responseText;
+
+
+                    liff.sendMessages([{
+                        type: 'text',
+                        text: x
+                    }]).then(function() {
+                        liff.closeWindow();
+                    }).catch(function(error) {
+                        window.alert("Error sending message: " + error);
+                    });
+
+                };
+                xhttp.open("POST", "https://stormy-spire-09445.herokuapp.com/liff", true);
+                xhttp.setRequestHeader("Content-Type", "application/json");
+                xhttp.send();
+
+            }
+            // liff.sendMessages([{
+            //     type: 'text',
+            //     text: x
+            // }]).then(function() {
+            //     liff.closeWindow();
+            // }).catch(function(error) {
+            //     window.alert("Error sending message: " + error);
+            // });
+    });
+
+}
